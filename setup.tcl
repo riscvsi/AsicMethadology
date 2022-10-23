20b26e177db50b52f48c49349b8297f7c8895961
riscvsi <56304509+riscvsi@users.noreply.github.com>
2022-10-23 17:13:56 +0530

    Create setup.tcl
    basic setup.tcl file for checking

---
+++ setup.tcl
@@ -0,0 +1,9 @@
+set RTLPath "/nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/RTL/"
+set RTLFile "/nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/RTL/module_i2c.v"
+set designName "module_i2c"
+set sdcFile "/nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/constraints/dtmf_recvr_core_gate.sdc"
+set effort medium
+set libFiles "/nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LIB/typical.lib /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LIB/typical.ecsm.lib /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LIB/pll.lib /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LIB/CDK_S256x16.lib /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LIB/CDK_S128x16.lib /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LIB/CDK_R512x16.lib"
+set libSearchPath "/nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LIB/"
+set lefFiles "/nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LEF/pll.lef /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LEF/gsclib045_tech.lef /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LEF/gsclib045_macro.lef /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LEF/CDK_S256x16.lef /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LEF/CDK_S128x16.lef /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LEF/CDK_R512x16.lef"
+set lefSearchPath "/nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/Genus_CUI_RAK/LEF/"
