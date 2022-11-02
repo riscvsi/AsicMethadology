4345c5dbabde616f389e230aba7b5b4c22d66193
riscvsi <56304509+riscvsi@users.noreply.github.com>
2022-11-02 19:15:04 +0530

    Update floorplan.tcl
    fixed innovus.tcl

--- scripts/innovus/floorplan.tcl
+++ scripts/innovus/floorplan.tcl
@@ -1,6 +1,19 @@
 
 source setup.tcl
 
+
+set conf_qxconf_file {NULL}
+set conf_qxlib_file {NULL}
+set defHierChar {/}
+set init_design_settop 0
+set init_gnd_net {VSS}
+set init_lef_file $lefFiles
+set init_mmmc_file scripts/innovus/mmmc${technology}.tcl}
+set init_pwr_net {VDD}
+set init_verilog synthesis/${designName}_synth.v
+set lsgOCPGainMult 1.000000
+
+
 set pnrDir "pnr"
 if {![file exists $pnrDir]} {
   file mkdir "pnr"
@@ -12,7 +25,7 @@ catch {cd $pnrDir}
 set_multi_cpu_usage -local_cpu $numCPUPnR -verbose
 
 ### need to generated viewDefinition_cui.tcl and use it here
-read_mmmc ../INPUT/viewDefinition_cui.tcl
+#read_mmmc ../INPUT/viewDefinition_cui.tcl
 ###
 
 
