09afda176ab21c5838ca5563d26d0a7aaeedad14
rcg <ramapriya.c.g@intel.com>
2022-11-28 10:58:26 -0800

    top script added

---
+++ scripts/innovus/topSoc.tcl
@@ -0,0 +1,65 @@
+
+source setup.tcl
+
+
+
+
+lappend libFiles ../riscvCoreSyntaCore1/ramInputs/sram_32_1024_max_1p8V_25C.lib
+lappend libMinFiles ../riscvCoreSyntaCore1/ramInputs/sram_32_1024_max_1p8V_25C.lib
+lappend libFiles /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/venvRcg/AsicMethadology/pnr/scr1_pipe_top.lib
+lappend libMinFiles /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/venvRcg/AsicMethadology/pnr/scr1_pipe_top.lib
+lappend lefFiles /nfs/site/disks/vmisd_vclp_efficiency/rcg/test/cadence/venvRcg/AsicMethadology/pnr/scr1_pipe_top.lef
+lappend lefFiles ../riscvCoreSyntaCore1/ramInputs/sram_32_1024.lef
+
+
+
+set conf_qxconf_file {NULL}
+set conf_qxlib_file {NULL}
+set defHierChar {/}
+set init_design_settop 0
+set init_gnd_net {VSS}
+set init_lef_file $lefFiles
+set init_mmmc_file [file normalize scripts/innovus/mmmc${technology}.tcl]
+set init_pwr_net {VDD}
+set init_verilog [file normalize synthesis/outputs/${designName}_synth.v]
+set powerIntent [file normalize synthesis/outputs/${designName}_synth1.upf]
+set lsgOCPGainMult 1.000000
+set init_design_setup ${designName}
+
+set pnrDir "pnr"
+if {![file exists $pnrDir]} {
+  file mkdir "pnr"
+  puts "Creating directory $pnrDir"
+}
+catch {cd $pnrDir}
+
+
+set_multi_cpu_usage -local_cpu 2 -verbose
+
+### need to generated viewDefinition_cui.tcl and use it here
+read_mmmc $init_mmmc_file
+###
+
+
+if {$designName == "scr1_pipe_top"} {
+    lappend lefFiles ../riscvCoreSyntaCore1/ramInputs/sram_32_1024.lef
+}
+read_physical -lef $lefFiles
+
+read_netlist ../riscvCoreSyntaCore1/src/top/scr1_top_ahb.v
+
+init_design
+#create_floorplan -site CoreSite -core_density_size 1 0.7 10 10 10 10
+
+read_power_intent -1801 ../scripts/genus/block.upf
+commit_power_intent -verbose
+
+
+
+check_power_domains -nets_missing_iso
+check_power_domains -nets_missing_shifter
+
+
+write_db top.inn
+
+exit
